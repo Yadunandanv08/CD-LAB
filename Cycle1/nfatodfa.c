12e8bb5793edb2a20692212a6e766145ff7ecdfe
yadu <yadunandanv08@gmail.com>
2025-10-28 12:17:44 +0530

    additions

--- Cycle1/nfatodfa.c
+++ Cycle1/nfatodfa.c
@@ -1,117 +1,107 @@
 #include<stdio.h>
 
-typedef struct {
-    int from;
-    char sym;
-    int to;
-} Transition;
+typedef struct{
+	int from;
+	char sym;
+	int to;
+}Transition;
 
-void sort(int arr[], int n){
-    for(int i = 0; i < n-1; i++){
-        for(int j = 0; j < n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+void sort(int array[], int size){
+	for(int i=0;i<size-1;i++){
+		for(int j=0;j<size-i-1;j++){
+			if(array[j]>array[j+1]){
+				int temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
 }
 
-int check_duplicate(int arr1[], int arr2[], int s1, int s2){
-    sort(arr1, s1);
-    sort(arr2, s2);
-    if(s1 != s2) return 0;
-    for(int i = 0; i < s1; i++){
-        if(arr1[i] != arr2[i]) return 0;
-    }
-    return 1;
+int isduplicate(int arr1[], int arr2[], int size1, int size2){
+	if(size1!=size2) return 0;
+	sort(arr1, size1);
+	sort(arr2, size2);
+	for(int i=0;i<size1;i++)
+		if(arr1[i]!=arr2[i]) return 0;
+	return 1;
 }
 
-int ifexists(int array[10], int value, int size){
-    for(int i = 0; i < size; i++){
-        if(array[i] == value) return 1;
+int alreadyexists(int array[], int value, int size){
+    for(int i=0;i<size;i++){
+        if(array[i]==value) return 1;
     }
     return 0;
 }
 
+int dfastates[10][10], statecount[10], curstate, numnewstates, numtrans, start, numfinal, finalstates[10], numsym;
+char symbols[10];
+Transition transitions[10];
+
 void main(){
-    int numstates, numsyms, states, numtrans, start, numfinal, finalstates[10], newstates[10][10], numnewstates, statesinnew[10];
-    char symbols[10];
-    Transition transitions[10];
 
-    printf("Enter number of states: ");
-    scanf("%d", &numstates);
-    printf("Enter number of symbols: ");
-    scanf("%d", &numsyms);
-    printf("Enter symbols: ");
-    for(int i = 0; i < numsyms; i++){
-        scanf(" %c", &symbols[i]);
-    }
-
-    printf("Enter number of transitions: ");
-    scanf("%d", &numtrans);
-    printf("Enter transitions (from symbol to):\n");
-    FILE *f = fopen("test.txt", "r");
-    for(int i = 0; i < numtrans; i++){
-        fscanf(f, "%d %c %d", &transitions[i].from, &transitions[i].sym, &transitions[i].to);
-    }
-
-    printf("Enter start state: ");
-    scanf("%d", &start);    
-    printf("Enter number of final states: ");
-    scanf("%d", &numfinal);
-    printf("Enter final states: ");
-    for(int i = 0; i < numfinal; i++){
-        scanf("%d", &finalstates[i]);
-    }
-
-    printf("Start state: %d\n", start);
-
-    newstates[0][0] = start;
-    statesinnew[0] = 1;
-    numnewstates = 1;
-    int curstate = 0;
-
-    while(curstate<numnewstates){
-        for(int j=0;j<numsyms;j++){
-            int result[10];
-            int resultsize=0;
-            for(int k=0;k<statesinnew[curstate];k++){
-                for(int l=0;l<numtrans;l++){
-                    if(newstates[curstate][k]==transitions[l].from && symbols[j]==transitions[l].sym){
-                        int tostate = transitions[l].to;
-                        int found=ifexists(result, tostate, resultsize);
-                        if(!found){
-                            result[resultsize++]=tostate;
-                        }
-                    }
-                }
-            }
-            int duplicate=0;
-            for(int m=0;m<numnewstates;m++){
-                if(check_duplicate(result, newstates[m], resultsize, statesinnew[m])){
-                    duplicate=1;
-                    break;
-                }
-            }
-            if(!duplicate && resultsize>0){
-                for(int m=0;m<resultsize;m++){
-                    newstates[numnewstates][m]=result[m];
-                }
-                statesinnew[numnewstates]=resultsize;
-                numnewstates++;
-            }
-        }
-        curstate++;
-    }
-
-    printf("New States:\n");
-    for(int i=0;i<numnewstates;i++){
-        printf("State %d: { ", i);
-        for(int j=0;j<statesinnew[i];j++){
-            printf("%d ", newstates[i][j]);
-        }
-        printf("}\n");
-    }
+	printf("Enter number of symbols: ");
+	scanf("%d", &numsym);
+	
+	printf("Enter symbols: ");
+	scanf("%s", symbols);
+	
+	printf("Enter number of transitions: ");
+	scanf("%d", &numtrans);
+	
+	printf("Enter start state: ");
+	scanf("%d", &start);
+	
+	printf("Enter number of final states:: ");
+	scanf("%d", &numfinal);
+	
+	printf("Enter final states:\n");
+	for(int i=0;i<numfinal;i++) scanf("%d", &finalstates[i]);
+	
+	FILE *f = fopen("test.txt", "r");
+	for(int i=0;i<numtrans;i++)
+		fscanf(f, "%d %c %d", &transitions[i].from, &transitions[i].sym, &transitions[i].to);
+	
+	dfastates[0][0] = start;
+	numnewstates=1;
+	statecount[0]=1;
+	curstate = 0;
+	
+	while(curstate<numnewstates){
+		for(int i=0;i<numsym;i++){
+			int result[10];
+			int rescount = 0;
+			for(int j=0;j<statecount[curstate];j++){
+				for(int k=0;k<numtrans;k++){
+					if(transitions[k].from == dfastates[curstate][j] && transitions[k].sym == symbols[i]){
+                        if(!alreadyexists(result, transitions[k].to, rescount))
+						    result[rescount++] = transitions[k].to;
+					}
+				}
+			}
+			
+			int duplicate = 0;
+			for(int l=0;l<numnewstates;l++){
+				if(isduplicate(result, dfastates[l], rescount, statecount[l])){
+					duplicate = 1;
+					break;
+				}
+			}
+			if(!duplicate && rescount>0){
+				for(int l=0;l<rescount;l++){
+					dfastates[numnewstates][l] = result[l];
+				}
+				statecount[numnewstates] = rescount;
+				numnewstates++;
+			}
+			
+		}
+		curstate++;
+	}
+	printf("\nNew DFA States:");
+	for(int i=0;i<numnewstates;i++){
+		printf("[");
+		for(int j=0;j<statecount[i];j++) printf("q%d", dfastates[i][j]);
+		printf("]\n");
+	}
 }
--- Cycle3/sr.c
+++ Cycle3/sr.c
@@ -1,70 +1,73 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<string.h>
 
-char stack[100];
-char buffer[100];
-int top = -1,reduced = 1;
+typedef struct{
+	char lhs;
+	char rhs[10];
+} Rule;
 
-void push(char value) {
-    stack[++top] = value;
+char expression[10], stack[10], input;
+int top = -1, numrules = 8;
+Rule rules[8];
+
+void push(char c){
+	stack[++top] = c;
 }
 
-char pop() {
-    return stack[top--];
+int pop(){
+	return stack[top--];
 }
 
-void reduce(){
+int reduce(){
+	for(int i=0;i<numrules;i++){
+		char rhs[10];
+		strcpy(rhs, rules[i].rhs);
+		
+		if(top>=strlen(rhs)-1){
+			char temp[10];
+			for(int j=0;j<strlen(rhs);j++) temp[j]=stack[top-strlen(rhs)+1+j];
+			temp[strlen(rhs)] = '\0';
+			if(strcmp(temp, rhs)==0){
+				for(int j=0;j<strlen(rhs);j++) pop();
+				push(rules[i].lhs);
+				for(int i=0;i<=top;i++) printf("%c", stack[i]);
+				printf("\tReduced %c --> %s\n", rules[i].lhs, rules[i].rhs);
+				return 1;
+			} 
+		}
+	}
+	return 0;
+}
+
+void main(){
+	printf("Enter Expression: ");
+	scanf("%s", expression);
 	
-	if(stack[top] == 'i'){
-		reduced = 1;
-		stack[top] = 'S';
-		return;
-	}
-	if (top > 1 && (stack[top] == 'S' && stack[top-1] == '+' && stack[top-2] == 'S')){
-		reduced = 1;
-		for(int i = 0; i < 2;i++) pop();
-		return;
-	}
-	if (top > 1 && (stack[top] == 'S' && stack[top-1] == '*' && stack[top-2] == 'S')){
-		reduced = 1;
-		for(int i = 0; i < 2;i++) pop();
-		return;
-	}
-    if (top > 1 && (stack[top] == ')' && stack[top-1] == 'S' && stack[top-2] == '(')){
-		reduced = 1;
-		for(int i = 0; i < 2;i++) pop();
-		return;
-	}
-}
+	FILE *f = fopen("grammar.txt", "r");
 
-int main(){
-    char expression[100];
-    printf("Enter the expression: ");
-    scanf("%s", expression);
-
-    int i = 0;
-    while(expression[i] != '\0'){
-        char c = expression[i];
-
-	while (reduced){
-		reduced = 0;
-		reduce();
+	for(int i=0;i<numrules;i++){
+		fscanf(f, " %c --> %s", &rules[i].lhs, rules[i].rhs);
 	}
 	
-	reduced =1;
-	push(c);
-	i++; 
-    }
-    
-    while(reduced){
-    	reduced = 0;
-    	reduce();
-    }
-    
-    if (top == 0 && stack[top] == 'S'){
-        printf("The expression is accepted.\n");
-    } else {
-        printf("The expression is not accepted.\n");
-    }
+	for(int i=0;expression[i]!='\0';i++){
+		input = expression[i];
+		if(top == -1) {
+			push(input);
+			for(int i=0;i<=top;i++) printf("%c", stack[i]);
+			printf("\tShifted %c\n", input);
+		}
+		else{
+			while(reduce());
+			push(input);
+			for(int i=0;i<=top;i++) printf("%c", stack[i]);
+			printf("\tShifted %c\n", input);
+		}
+	}
+	while(reduce());
 
-}
+	printf("Final Stack: ");
+	for(int i=0;i<=top;i++) printf("%c", stack[i]);
+	
+	if(top==0) printf("Parsed\n");
+	else printf("rejected\n");
+}   
